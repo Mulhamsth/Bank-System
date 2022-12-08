d756e04d5a4470e5a90f4c0badbfaa1f22e74cd0
Mulhamsth <mulhamsth@gmail.com>
2022-12-08 21:22:55 +0100

    Account management class

---
+++ Bank_System/Bank_System/Class1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Bank_System
+{
+    public class Account
+    {
+        //readonly
+        internal int ID;
+        internal string Name { get; set; }
+        internal int Balance { get; set; }
+
+        private string _email { get; set; }
+        internal string Email 
+        { 
+            get { return _email; } 
+            set
+            {
+                //A simple regex to validate string against email format and
+                //catch the most obvious syntax errors:
+
+                if (new Regex(@"^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$").Match(value).Success)
+                    this._email = value;
+                else
+                    throw new Exception("Invalid Email");
+            }
+        }
+
+        private string _password { get; set; }
+        internal string Password
+        {
+            get { return _password; }
+            set
+            {
+                /*
+                Has minimum 8 characters in length. Adjust it by modifying {8,}
+                At least one uppercase English letter. You can remove this condition by removing (?=.*?[A-Z])
+                At least one lowercase English letter.  You can remove this condition by removing (?=.*?[a-z])
+                At least one digit. You can remove this condition by removing (?=.*?[0-9])
+                At least one special character,  You can remove this condition by removing (?=.*?[#?!@$%^&*-])
+                */
+
+                if (new Regex(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$").Match(value).Success)
+                    _password = value;
+                else
+                    throw new Exception("Invalid Password");
+            }
+        }
+        public Account(int ID, string Name, string email, string password, int balance = 0)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Balance = balance;
+            this.Email = email;
+            this.Password = password;
+        }
+
+        public override string ToString()
+        {
+            /* for storing in Database
+             * e.g.:     1;Max Mustermann;mustermann;Maxmuster1234!;100
+             * the ; is used to make it readable for the Database when split it
+             */
+            return $"{ID};{Name};{_email};{_password};{Balance}";
+        }
+    }
+
+    public class AccountsTable
+    {
+        public List<Account> _accounts = new List<Account>();
+        List<Account> ExtractedAccounts = new List<Account>();
+        public int Count { get { return _accounts.Count; } }
+
+
+        #region Extracting Accounts
+        private bool Extracted = false;
+        public void ExtractAccounts(string filename)
+        {
+            StreamReader sr = new StreamReader(filename);
+            while(sr.Peek() > 0)
+            {
+                string line = sr.ReadLine();
+                string[] parts = line.Split(";");
+                int ID = int.Parse(parts[0]);
+                string Name = parts[1];
+                string Email = parts[2];
+                string Password = parts[3];
+                int Balance = int.Parse(parts[4]);
+                Account account = new Account(ID, Name, Email, Password, Balance);
+                ExtractedAccounts.Add(account);
+            }
+            sr.Close();
+        }
+       
+        public void AddExtractedAccounts()
+        {
+            if (Extracted)
+            {
+                foreach (Account account in ExtractedAccounts) { _accounts.Add(account); }
+                Extracted = false;
+                ExtractedAccounts.Clear();
+            }
+            return;
+        }
+        public string ShowExtractedAccounts(string seperator = "\n")
+        {
+            StringBuilder strb = new StringBuilder();
+            foreach (Account a in ExtractedAccounts)
+            {
+                strb.Append(a.ToString() + seperator);
+            }
+            string str = strb.ToString();
+            return str;
+        }
+        #endregion
+        public string ShowAccounts(string seperator = "\n")
+        {
+            StringBuilder strb = new StringBuilder();
+            foreach (Account a in _accounts)
+            {
+                strb.Append(a.ToString() + seperator);
+            }
+            string str = strb.ToString();
+            return str;
+        }
+        public void ExportAccounts(string filename) 
+        {
+            StreamWriter sw = new StreamWriter(filename);
+            foreach(Account account in _accounts) { sw.WriteLine(account); }
+            sw.Close();
+        }
+        public void AddAccount(Account account)
+        {
+            _accounts.Add(account);
+        }
+        public Account FindAccount(int id)
+        {
+            foreach(Account account in _accounts)
+            {
+                if(account.ID == id) return account;
+            }
+            return null;
+        }
+        public void RemoveAccount(Account account)
+        {
+            _accounts.Remove(account);
+        }
+        public bool DoesExist(int id)
+        {
+            if(FindAccount(id) == null) return false;
+            return true;
+        }
+        public void ModifyAccount(Account acc, string? Name, string? Email, string? password)
+        {
+            if(Name != null)
+                acc.Name = Name;
+            if(Email != null) 
+                acc.Email = Email;
+            if(password != null) 
+                acc.Password = password;
+        }
+    }
+
+    
+
+}
---
+++ Bank_System/Bank_System/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Bank_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AccountsTable At = new AccountsTable();
+
+            Account a = new Account(1,"Mulham Taylouni", "mulham@gmail.com", "Password!1234");
+            Account a2 = new Account(2, "Morhaf Taylouni", "morhaf@gmail.com", "Password!q53");
+            Account a3 = new Account(3, "Wiesinger Christian", "Wiesinger@gmail.com", "Password!423525");
+
+            At.AddAccount(a);
+            At.AddAccount(a2);
+            At.AddAccount(a3);
+
+            At.ExportAccounts("data.txt");
+
+            Console.WriteLine(At.ShowAccounts());
+            Console.WriteLine("_----------------------");
+            At.ExtractAccounts("data.txt");
+            Console.WriteLine(At.ShowExtractedAccounts("\n"));
+            Console.WriteLine("_----------------------");
+            At.RemoveAccount(At.FindAccount(2));
+            Console.WriteLine(At.ShowAccounts());
+            Console.WriteLine("------------------------");
+            At.ModifyAccount(At.FindAccount(3),null,null,"asw3Adda!124");
+            Console.WriteLine(At.ShowAccounts());
+        }
+    }
+}
